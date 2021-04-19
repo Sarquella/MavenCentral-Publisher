d1e87c890cde9c2592ad82951a6e4169ecdbccc6
Adrià Sarquella Farrés <adria@sarquella.dev>
2021-04-19 21:00:23 +0200

    Add config for plugin publishing (#2)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     kotlin("jvm") version "1.3.72"
     id("java-gradle-plugin")
     id("maven-publish")
+    id("com.gradle.plugin-publish") version "0.14.0"
 }
 
 group = "dev.sarquella"
@@ -21,6 +22,8 @@ gradlePlugin {
         create("publisherPlugin") {
             id = "dev.sarquella.mavencentral-publisher"
             implementationClass = "dev.sarquella.plugin.PublisherPlugin"
+            displayName = "MavenCentral-Publisher"
+            description = "A gradle plugin to easily upload libraries to MavenCentral for distribution."
         }
     }
 }
@@ -28,4 +31,10 @@ gradlePlugin {
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+pluginBundle {
+    website = "https://github.com/Sarquella/MavenCentral-Publisher"
+    vcsUrl = "https://github.com/Sarquella/MavenCentral-Publisher"
+    tags = listOf("library", "mavencentral", "publish", "sonatype")
 }
